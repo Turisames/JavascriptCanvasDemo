63ea5de1f3cb39729f623f57ba85dd2bfe444b8b
Turisames <turisames@gmail.com>
2017-04-04 00:53:52 +0300

    Now the ball spins in place, as if nailed on one of the edge.

--- behavior.js
+++ behavior.js
@@ -1,10 +1,16 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext("2d");
+var width = canvas.width;
+var height = canvas.height;
+var centerX = width/2;
+var centerY = height/2;
+var ray = 30;
+var time = 0;
 
-function drawCircle( centerX, centerY ) {
-  var radius = 70;
+function drawCircle( parX, parY ) {
+  var radius = 30;
   context.beginPath();
-  context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+  context.arc( parX, parY, radius, 0, 2 * Math.PI, false);
   context.fillStyle = 'green';
   context.fill();
   context.lineWidth = 5;
@@ -12,14 +18,29 @@ function drawCircle( centerX, centerY ) {
   context.stroke();
 }
 
-function spinBall() {
+// Takes time in ms, determines position accordingly.
+function spinBall( Time = 0 ) {
   var angle = 0;
-  posX = 0;
-  posY = 0;
+  var posX = 0;
+  var posY = 0;
+  // Note to self, uses radians.
+  posY = Math.sin( Time * Math.PI ) * ray + centerY;
+  posX = Math.cos( Time * Math.PI ) * ray + centerX;
+
+  //drawCircle( centerX, centerY );
+  drawCircle( posX, posY );
+}
+
+function drawOnTime() {
+  // Clear the canvas.
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  spinBall( time );
+  time += 1/8;
 }
 
 function main() {
-  drawCircle( 10, 10 );
+  //drawCircle( 10, 10 ); // Basic test
+  setInterval(drawOnTime, 40);
 }
 
 main();

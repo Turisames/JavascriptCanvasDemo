89b159f1a9496f996b7d997d557533ee3e83671c
Turisames <turisames@gmail.com>
2017-04-03 22:37:44 +0300

    The program now draws a ball in the top left corner of the canvas. I am going to take steps to make it do something more impressive.

--- behavior.js
+++ behavior.js
@@ -1,7 +1,8 @@
 var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+var context = canvas.getContext("2d");
 
 function drawCircle( centerX, centerY ) {
+  var radius = 70;
   context.beginPath();
   context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
   context.fillStyle = 'green';
@@ -9,6 +10,12 @@ function drawCircle( centerX, centerY ) {
   context.lineWidth = 5;
   context.strokeStyle = '#003300';
   context.stroke();
+}
+
+function spinBall() {
+  var angle = 0;
+  posX = 0;
+  posY = 0;
 }
 
 function main() {

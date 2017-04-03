c48005d85264afc4c5cc497e899641f786ac7cd5
Turisames <turisames@gmail.com>
2017-04-04 02:01:05 +0300

    The script now animates according to the size of the window, kind of. It doesn't automatically adjust to window size, but updating the page will do the trick.

--- behavior.js
+++ behavior.js
@@ -2,12 +2,18 @@
 
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext("2d");
+
+// Adjust canvas size.
+context.canvas.width = window.innerWidth;
+context.canvas.height = window.innerHeight;
 // Canvas dimensions.
 var width = canvas.width;
 var height = canvas.height;
 // Center of the canvas.
 var centerX = width/2;
 var centerY = height/2;
+// The radius of a ball.
+var radius = 30;
 // The radius of the circle along which balls spin.
 var ray = height/3;
 //
@@ -16,14 +22,13 @@ var time = 0;
 var sinexpos = -40;
 var cosinexpos = -120;
 // The modifier of the sine and cosine tracks.
-var sinepower = 150
+var sinepower = canvas.height * 1/9;
 // Speed of the animation.
 var pace = 50;
 
 // END of globals.
 
 function drawCircle( parX, parY, fillStyle = 'green' ) {
-  var radius = 30;
   context.beginPath();
   context.arc( parX, parY, radius, 0, 2 * Math.PI, false);
   context.fillStyle = fillStyle;
@@ -40,24 +45,29 @@ function spinBall( Time = 0 ) {
   var posY = centerY + Math.sin( Time * Math.PI ) * ray;
 
   drawCircle( posX, posY, 'pink' );
-  drawCircle( posY, posX, 'cyan' );
+  drawCircle( width - posX, posY, 'cyan' );
 }
 
 function sineWaveBall() {
-  var ypos = 150 + Math.sin( sinexpos ) * sinepower;
+  var ypos = centerY + Math.sin( sinexpos ) * sinepower;
   //alert( ypos );
   drawCircle( sinexpos, ypos, 'red' );
   //sinexpos += 5;
 }
 
 function cosineWaveBall() {
-  var ypos = 150 + Math.cos( sinexpos ) * sinepower;
+  var ypos = centerY + Math.cos( sinexpos ) * sinepower;
   //alert( ypos );
   drawCircle( sinexpos, ypos, 'blue' );
   //sinexpos += 5;
 }
 
 function drawOnTime() {
+  // Adjust canvas size.
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  sinepower = canvas.height * 1/9;
+
   // Clear the canvas.
   context.clearRect(0, 0, canvas.width, canvas.height);
   spinBall( time );
@@ -65,11 +75,11 @@ function drawOnTime() {
   cosineWaveBall();
   sinexpos += 5;
   cosinexpos += 5;
-  if (sinexpos > (40 + canvas.width) ) {
-    sinexpos = -40;
+  if (sinexpos > (radius + canvas.width) ) {
+    sinexpos = -radius;
   }
-  if ( cosinexpos > (40 + canvas.width) ) {
-    cosinexpos = -40;
+  if ( cosinexpos > (radius + canvas.width) ) {
+    cosinexpos = -radius;
   }
   time += 1/8;
 }

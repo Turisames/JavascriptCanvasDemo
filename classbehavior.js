c8efb9346cbf75ad7a5c21076506b376cc6eb5b5
Turisames <turisames@gmail.com>
2017-04-06 12:41:14 +0300

    Put in more functionality. Still work to left to do.

--- classbehavior.js
+++ classbehavior.js
@@ -1,3 +1,15 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext("2d");
+
+// Adjust canvas size.
+context.canvas.width = window.innerWidth;
+context.canvas.height = window.innerHeight;
+// Canvas dimensions.
+var width = canvas.width;
+var height = canvas.height;
+// Center of the canvas.
+var centerX = width/2;
+var centerY = height/2;
 
 class Circle {
   constructor( parX, parY, Rad = 30, Color = 'green' ) {
@@ -20,6 +32,24 @@ class Circle {
   setPos( parX, parY ){
     this.x = parX;
     this.y = parY;
+  }
+
+  clockwise(){
+    this.x = centerX + Math.cos( Time * Math.PI ) * ray;
+    this.y = centerY + Math.sin( Time * Math.PI ) * ray;
+  }
+
+  counterclock(){
+    this.x = width - (centerX + Math.cos( Time * Math.PI ) * ray);
+    this.y = centerY + Math.sin( Time * Math.PI ) * ray;
+  }
+
+  sineroute(){
+    this.y = centerY + Math.sin( sinexpos ) * sinepower;
+  }
+
+  cosineroute(){
+    this.y = centerY + Math.cos( sinexpos ) * sinepower;
   }
 
   update(){
